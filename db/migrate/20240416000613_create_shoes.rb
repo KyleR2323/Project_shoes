738f659fc0f9a73537b95f2d19bc9309802ed863
KyleB <kylebullanday1@gmail.com>
2024-04-15 19:07:16 -0500

    Created Shoe model

---
+++ db/migrate/20240416000613_create_shoes.rb
@@ -0,0 +1,17 @@
+class CreateShoes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :shoes do |t|
+      t.string :shoe_model
+      t.string :shoe_model
+      t.integer :quantity_available
+      t.string :gender
+      t.string :size
+      t.string :color
+      t.string :material
+      t.decimal :price
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_15_235354) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_15_235738) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_15_235354) do
     t.string "first_name"
     t.string "last_name"
     t.text "address"
+    t.integer "province_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
