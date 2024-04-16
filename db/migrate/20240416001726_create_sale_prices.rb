9f785535363f677a069a0f1f3fac92d9e5993c26
KyleB <kylebullanday1@gmail.com>
2024-04-15 19:17:44 -0500

    Created SalePrice Model

---
+++ db/migrate/20240416001726_create_sale_prices.rb
@@ -0,0 +1,11 @@
+class CreateSalePrices < ActiveRecord::Migration[7.1]
+  def change
+    create_table :sale_prices do |t|
+      t.decimal :sale_price
+      t.date :sale_date
+      t.string :sale_item
+
+      t.timestamps
+    end
+  end
+end
